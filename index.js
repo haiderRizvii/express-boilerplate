3fe4bc181cbc1141267fff4dd22db9afa0610d10
haiderRizvii <haider.rizvi@devsinc.com>
2023-05-20 18:16:31 +0500

    console colors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 
 const app = express();
 
@@ -30,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is in ${process.env.NODE_ENV} mode, and is listening on ${PORT}`
+    `Server is in ${process.env.NODE_ENV} mode, and is listening on ${PORT}`.yellow.bold
   );
 });
 
